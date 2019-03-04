fc59fab754716d646ef18b98a5a88b5ca92ea2d7
nelson guest <katalenelson@gmail.com>
2019-03-04 03:54:09 +0300

    first commit

--- app/src/main/java/com/theedge/nelsongkatale/pocketcompass/PocketCompass.java
+++ app/src/main/java/com/theedge/nelsongkatale/pocketcompass/PocketCompass.java
@@ -5,6 +5,7 @@ import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Sensor;
@@ -19,6 +20,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,13 +34,15 @@ import android.widget.Toast;
 
 public class PocketCompass extends AppCompatActivity implements SensorEventListener {
 
-    private boolean buttontoggle=false;
-    private ImageButton flashLightButton;
-    boolean isflash=false;
+//    private boolean buttontoggle=false;
+//    private ImageButton flashLightButton;
+////    boolean isflash=false;
+
     private Camera camera;
-
+    private Camera.Parameters parameters;
+    private ImageButton flashLightButton;
+    boolean isFlashLightOn = false;
     final int CAMERA_PERMISSION_CODE=1;
-
     // define the display assembly compass picture
     private ImageView image;
 
@@ -57,31 +61,54 @@ public class PocketCompass extends AppCompatActivity implements SensorEventListe
         compassHeading=(TextView)findViewById(R.id.directionHeading);
         mSensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         dial=(ImageView)findViewById(R.id.compassDial);
-        flashLightButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onClick(View v) {
-                int permission= PermissionChecker.checkSelfPermission(PocketCompass.this, Manifest.permission.CAMERA);
-                if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= 23 && permission!= PermissionChecker.PERMISSION_GRANTED){
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 0);
 
+        if (isFlashSupport()) {
+            camera = Camera.open();
+            parameters = camera.getParameters();
+        } else {
+            showNoFlashAlert();
+        }
+
+        flashLightButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                int permission=PermissionChecker.checkSelfPermission(PocketCompass.this, Manifest.permission.CAMERA);
+                if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && permission!= PermissionChecker.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 0);
                 }else{
-                    int buildSdkVersion=Build.VERSION.SDK_INT;
-                    if (!isflash) {
-                        if (!buttontoggle) {
-                            if (buildSdkVersion >= Build.VERSION_CODES.LOLLIPOP) {
-                                toggleOn();
-                            }
-                        } else {
-                            toggleOff();
-                        }
+                    if (!isFlashLightOn) {
+                        toggleOn();
                     } else {
-                        flashHardware();
+                        toggleOff();
                     }
                 }
             }
         });
 
+        Toolbar toolbar=(Toolbar)findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+
+    /**toolbar*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.about,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.about_app:
+                Intent intent=new Intent(this,AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected  void onResume(){
@@ -120,36 +147,30 @@ public class PocketCompass extends AppCompatActivity implements SensorEventListe
 
     }
 
-
     /**method sets flashlight on*/
-    @TargetApi(Build.VERSION_CODES.M)
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void toggleOn(){
-        CameraManager cameraManager=(CameraManager)getSystemService(Context.CAMERA_SERVICE);
-
-        try {
-            String flashID=cameraManager.getCameraIdList()[0];
-            cameraManager.setTorchMode(flashID,true);
-            buttontoggle=true;
+        try{
             flashLightButton.setImageResource(R.drawable.lighton);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isFlashLightOn=true;
+
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
-    /* method  sets flashlight off*/
-    @TargetApi(Build.VERSION_CODES.M)
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void toggleOff(){
-        CameraManager cameraManager=(CameraManager)getSystemService(Context.CAMERA_SERVICE);
-
-        try {
-            String flashID=cameraManager.getCameraIdList()[0];
-            cameraManager.setTorchMode(flashID,false);
-            buttontoggle=false;
+    /** method  sets flashlight off*/
+    private  void toggleOff(){
+        try{
             flashLightButton.setImageResource(R.drawable.lightoff);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            isFlashLightOn=false;
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
@@ -166,36 +187,22 @@ public class PocketCompass extends AppCompatActivity implements SensorEventListe
      * display's dialog box when device doesn't support flash hardware
      * @return true when device doesn't have flash
      */
-    private boolean flashHardware(){
-        AlertDialog.Builder builder=new AlertDialog.Builder(PocketCompass.this);
-        builder.setTitle("Error!");
-        builder.setMessage("Device doesn't support flash");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                finish();
-            }
-        });
-        builder.show();
+    private void showNoFlashAlert() {
+        new AlertDialog.Builder(this)
+                .setMessage("Device hardware does not support flashlight!")
+                .setIcon(android.R.drawable.ic_dialog_alert).setTitle("Error")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 
-        return true;
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                }).show();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==CAMERA_PERMISSION_CODE){
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
-                Toast.makeText(PocketCompass.this,"Permission Granted",Toast.LENGTH_SHORT).show();
-
-            }else{
-
-                Toast.makeText(PocketCompass.this,"Permission Denied ! ",Toast.LENGTH_SHORT).show();
-            }
-
-        }
+    private boolean isFlashSupport() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
 }
